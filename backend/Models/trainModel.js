993d126360fc2727f260918ded00798e5e112ffe
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-06-10 23:21:08 +0530

    Update trainModel.js

--- backend/Models/trainModel.js
+++ backend/Models/trainModel.js
@@ -2,4 +2,4 @@ const seatSchema = new mongoose.Schema({
   number: { type: Number, required: true },
   row: { type: Number, required: true },
   isBooked: { type: Boolean, default: false },
-});
+}); 
