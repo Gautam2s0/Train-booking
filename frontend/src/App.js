b17827b39f8b6618072a37170e3423071783b05a
Gautam Sonkar <gautamsonkar334@gmail.com>
2023-06-10 15:40:44 +0530

    train

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Train } from './Components/Pages/Train';
+
+function App() {
+  return (
+    <div className="App">
+      <Train/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Pages/Train.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Box, Heading, Stack} from "@chakra-ui/react"
+
+export const Train = () => {
+  return (
+    <Stack>
+      <Heading>Train
+        <Stack className='leftSide'>
+          <Box>
+            <Heading>Train Booking System</Heading>
+          </Box>
+        </Stack>
+        <Stack className='rightSide'>
+
+        </Stack>
+      </Heading>
+    </Stack>
+  )
+}
