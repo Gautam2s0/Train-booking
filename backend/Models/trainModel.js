bdfad76e2f8e7799119cf7b457dd708ec82e6533
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-06-11 16:10:09 +0530

    Delete backend directory

--- backend/Models/trainModel.js
+++
@@ -1,6 +0,0 @@
-const seatSchema = new mongoose.Schema({
-  number: { type: Number, required: true },
-  row: { type: Number, required: true },
-  isBooked: { type: Boolean, default: false },
-}); 
- 
